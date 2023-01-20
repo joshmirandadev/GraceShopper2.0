36e9b9ea332440f978c2b8286e319d72bd9bfbd9
Joshua <joshmatimiranda@gmail.com>
2023-01-20 16:43:28 -0600

    entered actual products into Commerece and implemented into products component. Solved html rendering concern in descriptions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 
 // import Products from "./components/Products/Products";
 // import Navbar from "./components/NavBar/Navbar";
@@ -6,10 +7,22 @@ import React from "react";
 import { Products, Navbar } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   );
 };
